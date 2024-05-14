3c7f6fe8fcc5740ae9ab084c79b044c20ffd614b
chelond <142936713+chelond@users.noreply.github.com>
2024-05-14 22:18:11 +0400

    new

--- bot/handlers/user/main.py
+++ bot/handlers/user/main.py
@@ -47,7 +47,9 @@ def register_user_handlers(dp: Dispatcher):
     async def file(message: types.Message):
         buttons = [
             [
-                {"text": "Алехин", "callback_data": "OS"}
+                {"text": "Алехин", "callback_data": "OS"},
+                {"text": "Терехин", "callback_data": "BD"},
+                {"text": "ОАиП", "callback_data": "OAIP"}
             ]
         ]
         await message.answer(text='Выбери предмет/препода который хочешь найти', reply_markup={"inline_keyboard": buttons})
@@ -56,6 +58,31 @@ def register_user_handlers(dp: Dispatcher):
     async def buttonOS_handler(call: types.CallbackQuery):
         document = open('bot/handlers/user/file/Sbornik_LR_po_OS_IB.doc', 'rb')
         await bot.send_document(call.message.chat.id, document=document, caption="Алехин")
+
+    @dp.callback_query_handler(text="BD")
+    async def button_bd_handler(call: types.CallbackQuery):
+        media = types.MediaGroup()
+        media.attach_document(types.InputFile('bot/handlers/user/file/TK_1.docx'), caption="ТК1")
+        media.attach_document(types.InputFile('bot/handlers/user/file/TK_2.docx'), caption='ТК2')
+        media.attach_document(types.InputFile('bot/handlers/user/file/TK__3.docx'), caption='ТК3')
+        media.attach_document(types.InputFile('bot/handlers/user/file/TK_4.docx'), caption='ТК4')
+        media.attach_document(types.InputFile('bot/handlers/user/file/TK_5.docx'), caption='ТК5')
+        media.attach_document(types.InputFile('bot/handlers/user/file/TK_6.docx'), caption='ТК6')
+        media.attach_document(types.InputFile('bot/handlers/user/file/TK_7.docx'), caption='ТК7')
+        media.attach_document(types.InputFile('bot/handlers/user/file/TK_8.docx'), caption='ТК8')
+        media.attach_document(types.InputFile('bot/handlers/user/file/TK_9.docx'), caption='ТК9')
+
+        await bot.send_media_group(call.message.chat.id, media=media)
+
+    @dp.callback_query_handler(text="OAIP")
+    async def button_oaip_handler(call: types.CallbackQuery):
+        document = open('bot/handlers/user/file/Сборник_практических_занятий_ОАиП_28_48.pdf', 'rb')
+        await bot.send_document(call.message.chat.id, document=document, caption="Лабы C#")
+
+    @dp.message_handler(commands='mehebek')
+    async def mehebek(message: types.Message):
+        photo_path = 'bot/handlers/user/file/photo_2024-05-01_16-26-49.jpg'  # Укажите путь к файлу на вашем компьютере
+        await message.answer_photo(types.InputFile(photo_path), caption='Всем хорошего мэхэбэка!')
 
 
 
